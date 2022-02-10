b40c2085460fb30a310e5d7fe296173e72b07222
HELTON SYCLUS <84809618+heltonsyclus@users.noreply.github.com>
2022-02-10 09:35:08 -0300

    layouts

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,8 +1,14 @@
 const serviceLogin = require("../json/serviceLogin.json");
+const seriveLayout = require("../json/serviceLayouts.json");
 class UserController {
   loginGet(req, res) {
     return res.status(200).json({
       serviceLogin,
+    });
+  }
+  layoutGet(req, res) {
+    return res.status(200).json({
+      seriveLayout,
     });
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,5 @@ const UserController = require("./app/controllers/UserController");
 const routes = new Router();
 
 routes.get("/login", UserController.loginGet);
-
+routes.get("/layout", UserController.layoutGet);
 module.exports = routes;
