d93f64ff63b9fb782031971659044e15a80f008d
HELTON SYCLUS <84809618+heltonsyclus@users.noreply.github.com>
2022-02-10 10:40:26 -0300

    teste corpo

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,6 +1,7 @@
 const serviceLogin = require("../json/serviceLogin.json");
 const seriveLayout = require("../json/serviceLayouts.json");
 class UserController {
+  //get
   loginGet(req, res) {
     return res.status(200).json({
       serviceLogin,
@@ -10,6 +11,10 @@ class UserController {
     return res.status(200).json({
       seriveLayout,
     });
+  }
+  //post
+  testPost(req, res) {
+    console.log(req, body);
   }
 }
 module.exports = new UserController();
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,10 @@ const { Router } = require("express");
 const UserController = require("./app/controllers/UserController");
 
 const routes = new Router();
-
+//get
 routes.get("/login", UserController.loginGet);
 routes.get("/layout", UserController.layoutGet);
+
+//post
+routes.post("/teste", UserController.testPost);
 module.exports = routes;
